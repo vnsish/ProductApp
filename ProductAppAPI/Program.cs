2671043da2d6f69f38696fc44f6ae68ec70e4beb
vinicius <vinicius.ishioka@gmail.com>
2021-11-04 14:04:30 -0300

    Added DB seeding when empty, sorting icons.

--- ProductAppAPI/Program.cs
+++ ProductAppAPI/Program.cs
@@ -2,10 +2,12 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ProductAppAPI.Data;
 
 namespace ProductAppAPI
 {
@@ -13,7 +15,24 @@ namespace ProductAppAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ProductAppAPIContext>();
+                    if (!context.Product.Any()) DataSeed.SeedDB(context);
+                }
+                catch (Exception e)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(e, "Error seeding database.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
