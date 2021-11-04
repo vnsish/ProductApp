7c5e7636a2d7d80de974359a364c3d19565be516
vinicius <vinicius.ishioka@gmail.com>
2021-11-04 14:05:01 -0300

    Revert "Added DB seeding when empty, sorting icons."
    This reverts commit 2671043da2d6f69f38696fc44f6ae68ec70e4beb.

--- ProductAppAPI/Program.cs
+++ ProductAppAPI/Program.cs
@@ -2,12 +2,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using ProductAppAPI.Data;
 
 namespace ProductAppAPI
 {
@@ -15,24 +13,7 @@ namespace ProductAppAPI
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var context = services.GetRequiredService<ProductAppAPIContext>();
-                    if (!context.Product.Any()) DataSeed.SeedDB(context);
-                }
-                catch (Exception e)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(e, "Error seeding database.");
-                }
-            }
-
-            host.Run();
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
